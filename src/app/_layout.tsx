d270d3d92a1afe883e73c60fad37500602c15919
Gabriel <gabrodrigues0702@gmail.com>
2025-10-03 15:31:17 -0300

    enhance layout and styling for Home screen with improved tab navigation and welcome message

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,8 @@ import {Tabs} from "expo-router";
 
 export default function Layout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{headerShown: false, title: 'Home'}} />
+        <Tabs screenOptions={{ tabBarActiveTintColor: '#000000', tabBarStyle: { backgroundColor: '#ffffff', height:50, justifyContent:"center"} }}>
+            <Tabs.Screen name="index"  options={{headerShown: false, headerTintColor:"#000000", title: 'Home'}} />
             <Tabs.Screen name="profile" options={{headerShown: false, title: 'Perfil'}} />
         </Tabs>
     );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,28 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {Stack} from 'expo-router';
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#000000',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 16,
+        color: '#000',
+    },})
 
 export default function Index() {
   return (
 
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <Text>Welcome to the Home Screen!</Text>
-            <Text>This is the main entry point of the app.</Text>
-            <Text>Use the navigation to explore different sections.</Text>
+        <View style={styles.container}>
+            <View style={{ backgroundColor:"#fff", height:"60%", borderRadius:20}} >
+            <Text style={styles.title}>Welcome to the Home Screen!</Text>
+            <Text style={styles.title}>This is the main entry point of the app.</Text>
+            <Text style={styles.title}>Use the navigation to explore different sections.</Text></View>
         </View>
         
     )}
