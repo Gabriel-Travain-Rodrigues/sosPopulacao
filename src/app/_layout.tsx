60273555c79afda1ddc3362f4024d88d4af7c39d
Gabriel <gabrodrigues0702@gmail.com>
2025-11-27 08:47:17 -0300

    AAAAAAAAAAAAAAAAAAAAAAA

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,42 +3,43 @@ import { Ionicons } from "@expo/vector-icons";
 import { UserProvider } from "../context/userContext";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-
-
 export default function Layout() {
   return (
     <SafeAreaProvider>
       <UserProvider>
-    <Drawer
-      screenOptions={{
-        headerShown: false, // o header fica falso porque você já fez o topo customizado no index.js
-        drawerActiveTintColor: "#e63946",
-        drawerInactiveTintColor: "#333",
-        drawerLabelStyle: { fontSize: 16 },
-      }}
-    >
-      {/* Tela inicial */}
-      <Drawer.Screen
-        name="index"
-        options={{
-          title: "Início",
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
+        <Drawer
+          screenOptions={{
+            headerShown: false,
+            drawerActiveTintColor: "#e63946",
+            drawerInactiveTintColor: "#333",
+            drawerLabelStyle: { fontSize: 16 },
+          }}
+        >
+          <Drawer.Screen
+            name="index"
+            options={{
+              title: "Início",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home-outline" size={size} color={color} />
+              ),
+            }}
+          />
 
-      {/* Tela de perfil */}
-      <Drawer.Screen
-        name="profile"
-        options={{
-          title: "Perfil",
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="person-circle-outline" size={size} color={color} />
-          ),
-        }}
-      />
-    </Drawer>
-    </UserProvider>
+          <Drawer.Screen
+            name="profile"
+            options={{
+              title: "Perfil",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons
+                  name="person-circle-outline"
+                  size={size}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Drawer>
+      </UserProvider>
     </SafeAreaProvider>
-  )}
+  );
+}
